9ac97a05cbf876526ab0b405bc4b62f903ebf7de
Brian Rudolph <brian.rudolph303@gmail.com>
2020-08-19 09:17:59 -0600

    Initial look

---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Note() {
+    return (
+        <div className="note">
+            <h1>Note</h1>
+            <p>This is some note description</p>
+        </div>
+    );
+}
+
+export default Note;
