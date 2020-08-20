4602dd7677640b14f8323988f0a29c6a771f5ffc
Brian Rudolph <brian.rudolph303@gmail.com>
2020-08-20 10:15:55 -0600

    Initial Note display feature

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 
-function Note() {
+
+function Note(props) {
     return (
         <div className="note">
-            <h1>Note</h1>
-            <p>This is some note description</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     );
 }
