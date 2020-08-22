3fede3c0f1dbfe0e1f2ab58eebc1154792698fec
Brian Rudolph <brian.rudolph303@gmail.com>
2020-08-22 12:12:22 -0600

    Add delete note button

---
+++ src/components/Delete.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Delete() {
+
+    return (
+        <button>delete</button>
+    );
+
+}
+
+export default Delete;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Delete from "./Delete";
 
 
 
@@ -7,6 +8,7 @@ function Note(props) {
         <div className="note">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
+            <Delete />
         </div>
     );
 }
