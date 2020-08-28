418497dc6129e36596d4afbbd8cbee93289d9acc
Brian Rudolph <brian.rudolph303@gmail.com>
2020-08-28 16:17:28 -0600

    Add JQuery fadeIn

--- src/components/Addnote.jsx
+++ src/components/Addnote.jsx
@@ -3,8 +3,9 @@ import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import Zoom from '@material-ui/core/Zoom';
 
-function Addnote(props) {
 
+
+function Addnote(props) {
   
     const [titleField, setTitleFieldType] = useState({inputType: "hidden", rows : 0, zoomIn: false, textDescription: "Make a note..."});
     const [aNote, setNote] = useState({
@@ -41,18 +42,21 @@ function Addnote(props) {
         });
     }
 
+    
+    
 
     return (
         <div>
         <form onSubmit={handleFormSubmit} className="create-note">
         <input name="title" type={titleField.inputType} onChange={handleNote}  placeholder="Title" value={aNote.title} ></input>
             <textarea name="content" onChange={handleNote} onClick={displayTitleInput} rows={titleField.rows} cols="50" placeholder={titleField.textDescription} value={aNote.content}></textarea>
-            
             <Zoom in={titleField.zoomIn}><Fab type="submit" ><AddIcon /></Fab></Zoom>
-            </form>        
-        </div>
-        
+            </form>     
+        </div>   
+          
     );
+
+    
 
 }
 
